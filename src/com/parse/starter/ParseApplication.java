faa5c164ad8b4422cb49bf4c725ba3d1509191e8
shanmarkus <shanmarkus@gmail.com>
2014-06-10 18:32:29 +0700

    Git Init

---
+++ src/com/parse/starter/ParseApplication.java
@@ -0,0 +1,26 @@
+package com.parse.starter;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+
+import android.app.Application;
+
+public class ParseApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		Parse.initialize(this, "4EkDooudMyzhEGlZLQbGBk1fI1KSy5q4HKnXST2E",
+				"7XLkG5yFifLaSqZlGHeN3syJyflphTemixCmVV7c");
+
+		ParseUser.enableAutomaticUser();
+		ParseACL defaultACL = new ParseACL();
+
+		defaultACL.setPublicReadAccess(true);
+
+		ParseACL.setDefaultACL(defaultACL, true);
+	}
+
+}
