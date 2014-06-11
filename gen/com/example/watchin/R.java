9955aed2fed9724d121b5f5064f21af124776c15
shanmarkus <shanmarkus@gmail.com>
2014-06-11 21:19:02 +0700

    Fixing Layout

--- gen/com/example/watchin/R.java
+++ gen/com/example/watchin/R.java
@@ -1089,7 +1089,7 @@ containing a value of this type.
     }
     public static final class id {
         public static final int ButtonAdd=0x7f050040;
-        public static final int SearchButton=0x7f050046;
+        public static final int SearchButton=0x7f05004a;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1098,14 +1098,15 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f050023;
         public static final int action_bar_title=0x7f050022;
         public static final int action_context_bar=0x7f05001d;
-        public static final int action_example=0x7f05004f;
+        public static final int action_example=0x7f050053;
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05004e;
+        public static final int action_settings=0x7f050052;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int buttonStatus=0x7f050044;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1118,6 +1119,8 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int friendProfilePicture=0x7f050046;
+        public static final int friendUserName=0x7f050045;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
@@ -1125,22 +1128,22 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int imagePlaceBackground=0x7f050041;
         public static final int imageViewFriend=0x7f050043;
-        public static final int listFriend=0x7f050049;
+        public static final int listFriend=0x7f05004d;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
-        public static final int loginButton=0x7f05004d;
+        public static final int loginButton=0x7f050051;
         public static final int middle=0x7f050012;
         public static final int navigation_drawer=0x7f05003f;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
         public static final int pager=0x7f05003d;
-        public static final int passwordField=0x7f05004c;
+        public static final int passwordField=0x7f050050;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
-        public static final int relativeLayout1=0x7f050044;
-        public static final int relativeLayout2=0x7f050047;
+        public static final int relativeLayout1=0x7f050048;
+        public static final int relativeLayout2=0x7f05004b;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1151,7 +1154,7 @@ containing a value of this type.
         public static final int search_plate=0x7f050036;
         public static final int search_src_text=0x7f050037;
         public static final int search_voice_btn=0x7f05003b;
-        public static final int section_label=0x7f05004a;
+        public static final int section_label=0x7f05004e;
         public static final int shortcut=0x7f05002e;
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
@@ -1159,14 +1162,15 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textFindFriend=0x7f050045;
-        public static final int textTitleFollowing=0x7f050048;
+        public static final int textFindFriend=0x7f050049;
+        public static final int textRecentActivityTitle=0x7f050047;
+        public static final int textTitleFollowing=0x7f05004c;
         public static final int textUserName=0x7f050042;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
-        public static final int usernameField=0x7f05004b;
+        public static final int usernameField=0x7f05004f;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1220,14 +1224,16 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_add_friend=0x7f030018;
-        public static final int activity_friend_information=0x7f030019;
-        public static final int activity_main=0x7f03001a;
-        public static final int fragment_add_friend=0x7f03001b;
-        public static final int fragment_friend_information=0x7f03001c;
-        public static final int fragment_main=0x7f03001d;
-        public static final int fragment_navigation_drawer=0x7f03001e;
-        public static final int main=0x7f03001f;
-        public static final int support_simple_spinner_dropdown_item=0x7f030020;
+        public static final int activity_friend_detail=0x7f030019;
+        public static final int activity_friend_information=0x7f03001a;
+        public static final int activity_main=0x7f03001b;
+        public static final int fragment_add_friend=0x7f03001c;
+        public static final int fragment_friend_detail=0x7f03001d;
+        public static final int fragment_friend_information=0x7f03001e;
+        public static final int fragment_main=0x7f03001f;
+        public static final int fragment_navigation_drawer=0x7f030020;
+        public static final int main=0x7f030021;
+        public static final int support_simple_spinner_dropdown_item=0x7f030022;
     }
     public static final class menu {
         public static final int global=0x7f0c0000;
@@ -1290,7 +1296,11 @@ containing a value of this type.
         public static final int friend_information_friend_list_title=0x7f0a002f;
         public static final int friend_information_hint=0x7f0a002d;
         public static final int friend_information_imagebutton_desc=0x7f0a002e;
+        public static final int friend_relation_status=0x7f0a0035;
         public static final int hello_world=0x7f0a0016;
+        public static final int home_recent_activity_title=0x7f0a0037;
+        public static final int home_username=0x7f0a0036;
+        public static final int image_description_testing=0x7f0a0034;
         public static final int item_detail_delete_item=0x7f0a0022;
         public static final int item_detail_description=0x7f0a001a;
         public static final int item_detail_edit_button=0x7f0a0021;
