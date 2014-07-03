2a064af027da4a767a3a41cb734f46b7b8a526de
shanmarkus <shanmarkus@gmail.com>
2014-07-03 18:59:41 +0700

    Adding DangerActivity.class

---
+++ src/com/example/watchin/DangerActivity.java
@@ -0,0 +1,15 @@
+package com.example.watchin;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class DangerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_danger);
+	}
+}
