2280217fc61f2bd2d7effb96b47cb13f001db967
shanmarkus <shanmarkus@gmail.com>
2014-07-04 16:14:31 +0700

    save false

--- src/com/example/watchin/AddFriend.java
+++ src/com/example/watchin/AddFriend.java
@@ -59,18 +59,7 @@ public class AddFriend extends ActionBarActivity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int id = item.getItemId();
-//		if (id == R.id.action_edit_profile) {
-//			Intent intent = new Intent(this, EditProfile.class);
-//			startActivity(intent);
-//		}
-//		// Log out menu item
-//		else if (id == R.id.action_logout) {
-//			ParseUser.logOut();
-//			Intent intent = new Intent(this, LoginActivity.class);
-//			intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//			startActivity(intent);
-//		}
+
 		return super.onOptionsItemSelected(item);
 	}
 
